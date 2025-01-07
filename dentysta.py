968688c62729c5278e58c83af41d5f7054406a88
Łukasz Tugeman <tugeman.lukasz@gmail.com>
2025-01-07 18:35:54 +0100

    Add wyswietl function

--- dentysta.py
+++ dentysta.py
@@ -3,13 +3,16 @@ import os
 
 DANE_SCIEZKA = "dane/rejestrowanie.txt"
 
-def wczytaj(sciezka):
+def wczytaj(sciezka:str) ->dict:
     dane = {}
     if os.path.exists(sciezka):
         with open(sciezka, encoding=4) as plik:
             dane = json.load(plik)
     return dane
 
-def zapis(dane, sciezka):
+def zapis(dane:dict, sciezka:str):
     with open(sciezka, "w" ,encoding=4) as plik:
         json.dump(dane, plik)
+
+def wyswietl():
+    print(wczytaj(DANE_SCIEZKA))
