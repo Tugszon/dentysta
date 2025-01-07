27b9210402d45df6ce2bb16513253e5c8bba2ee9
Łukasz Tugeman <tugeman.lukasz@gmail.com>
2025-01-07 19:26:49 +0100

    Added dodaj and main functions

--- dentysta.py
+++ dentysta.py
@@ -1,4 +1,5 @@
 import json
+import math
 import os
 
 DANE_SCIEZKA = "dane/rejestrowanie.txt"
@@ -6,13 +7,46 @@ DANE_SCIEZKA = "dane/rejestrowanie.txt"
 def wczytaj(sciezka:str) ->dict:
     dane = {}
     if os.path.exists(sciezka):
-        with open(sciezka, encoding=4) as plik:
+        with open(sciezka) as plik:
             dane = json.load(plik)
     return dane
 
 def zapis(dane:dict, sciezka:str):
-    with open(sciezka, "w" ,encoding=4) as plik:
-        json.dump(dane, plik)
+    with open(sciezka, "w") as plik:
+        json.dump(dane, plik, indent=4)
 
 def wyswietl():
-    print(wczytaj(DANE_SCIEZKA))
+    return wczytaj(DANE_SCIEZKA)
+
+def dodaj(nowe_dane):
+    dane = wczytaj(DANE_SCIEZKA)
+    dane.update(nowe_dane)
+    zapis(dane, DANE_SCIEZKA)
+
+
+def main():
+    if not os.path.exists(DANE_SCIEZKA):
+        zapis({}, DANE_SCIEZKA)
+    indeks = 1 
+    while True:
+        opcja = input("Podaj co chcesz zrobic: ")
+        match opcja:
+            case "wyswietl":
+                print(wyswietl())
+            case "dodaj":
+                imie = input("Podaj imie: ")
+                nazwisko = input("Podaj nazwisko: ")
+                lata = input("Podaj wiek: ")
+                nowe_dane = {
+                    indeks: {
+                            "imie":imie,
+                            "nazwisko":nazwisko,
+                            "lata":lata
+                        }
+                    }
+                dodaj(nowe_dane)
+                indeks = indeks+1
+            case "exit":
+                break
+
+main()
